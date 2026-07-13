70ac73abfc95519e1a75bc88cf6f5f44add8f5c0
Andres Perdomo <136643768+NRDS92@users.noreply.github.com>
2026-07-13 10:54:33 +0200

    Update password reset URL to use static domain

--- apps/backend/src/modules/auth/utils/email.ts
+++ apps/backend/src/modules/auth/utils/email.ts
@@ -57,7 +57,7 @@ export const sendPasswordResetEmail = async (
     token: string
 ) => {
 
-    const url = `${process.env.FRONTEND_URL}/reset-password/${token}`;
+    const url = `https://www.veci-latin.com/reset-password/${token}`;
 
     await resend.emails.send({
         from: process.env.EMAIL_FROM!,
