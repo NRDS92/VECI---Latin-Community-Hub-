5653d9c5af210effd26e88918340f92d777354d0
Your Name <you@example.com>
2026-07-27 11:04:23 +0200

    fix: admin routes to regect event

--- apps/backend/src/modules/admin/admin.routes.ts
+++ apps/backend/src/modules/admin/admin.routes.ts
@@ -7,9 +7,15 @@ router.get(
     "/events/pending",
     adminController.getPendingEvents
 );
+
 router.patch(
     "/events/:id/approve",
     adminController.approveEvent
+);
+
+router.patch(
+    "/events/:id/reject",
+    adminController.rejectEvent
 );
 
 export default router;
